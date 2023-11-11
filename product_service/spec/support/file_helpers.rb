2573ebe0e7b78b7394abcccbf36f448ccf8f8a31
Magesh <pingmagesh@gmail.com>
2023-11-12 01:02:28 +0530

    tried some small improvements to file_helper

--- product_service/spec/support/file_helpers.rb
+++ product_service/spec/support/file_helpers.rb
@@ -1,19 +1,16 @@
-# spec/support/file_helpers.rb
 module FileHelpers
   def generate_test_image(name: 'test_image.jpg', content_type: 'image/jpeg', size: 1.kilobyte)
-    # file = Tempfile.new([name.split('.').first, File.extname(name)], binmode: true)
     file = Tempfile.new([name.split('.').first, File.extname(name)])
     file.binmode
-    file.write(SecureRandom.random_bytes(size)) while file.size < size
 
-    # raise "Test file is not large enough" unless file.size > 5.megabytes
+    while file.size < size
+      remaining_size = size - file.size
+      file.write(SecureRandom.random_bytes([remaining_size, 1024].min))
+    end
 
     file.rewind
-    p file
-    # Verify file size
-    raise "File size is not correct" unless file.size == size
+    raise "File size is not correct" unless file.size <= size
 
-    # Create the uploaded file object
     uploaded_file = ActionDispatch::Http::UploadedFile.new(
       filename: name,
       type: content_type,
