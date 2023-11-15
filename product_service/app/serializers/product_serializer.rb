2e1827a53e0eeb6f2dabc247473bcfda285c3857
Magesh <pingmagesh@gmail.com>
2023-11-15 19:18:05 +0530

    product serializer to return [] when there is no images

--- product_service/app/serializers/product_serializer.rb
+++ product_service/app/serializers/product_serializer.rb
@@ -5,6 +5,7 @@ class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :description, :image_urls
 
   def image_urls
-    object.images.map { |image| url_for(image) if image }
+    object.images.map { |image| url_for(image) if image } || []
   end
+
 end
